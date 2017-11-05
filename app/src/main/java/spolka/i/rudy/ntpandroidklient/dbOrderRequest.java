2f4c78114288dcdf0da7dc00ec1be8800b374caf
I2ufu5 <gamp3r@gmail.com>
2017-11-05 15:26:23 +0100

    Dodany ekran zamawiania w ktorym nie dziaa tylko przycisk skladania zamowienia bo nie przygotowalem skryptu w php-ie
    Trzeba przekazac z activity do order userID

---
+++ app/src/main/java/spolka/i/rudy/ntpandroidklient/dbOrderRequest.java
@@ -0,0 +1,38 @@
+package spolka.i.rudy.ntpandroidklient;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Rufus on 11/3/2017.
+ */
+
+public class dbOrderRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URL = "https://ntpprojekt.000webhostapp.com/ZamowOrder.php";                       // pehap nie opisany !!!!!!!!
+    private Map<String, String> params;
+
+    public dbOrderRequest(int userID, int productCount1,int productCount2, int productCount3, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener,null);
+        params = new HashMap<>();
+        params.put("userID",String.valueOf(userID));
+        params.put("prooductCount1",String.valueOf(productCount1));
+        params.put("prooductCount2",String.valueOf(productCount2));
+        params.put("prooductCount3",String.valueOf(productCount3));
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
+
+
+}
